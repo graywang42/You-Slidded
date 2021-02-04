99c8ca463d9d4f1fde1cd1c341e66b8aba23df1f
graywang42 <graywang42@gmail.com>
2021-02-03 21:47:55 -0800

    Added Tilemap Colliders, Basic Player Movement Physics

---
+++ You Slidded/Assets/You.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class You : MonoBehaviour
+{
+
+    public float speed = 7;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+}
