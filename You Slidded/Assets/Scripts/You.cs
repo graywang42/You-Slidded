47168b3b7b8a996ac52ff05e2a8e5aa1a0d3b057
graywang42 <graywang42@gmail.com>
2021-02-10 00:53:00 -0800

    Changed explosion mechanism to jump mechanism

--- You Slidded/Assets/Scripts/You.cs
+++ You Slidded/Assets/Scripts/You.cs
@@ -24,85 +24,56 @@ public class You : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.W))
         {
-            int slideDist = GetSlideDist(Vector2.up);
-            transform.position = transform.position + Vector3.up * slideDist;
             transform.rotation = Quaternion.Euler(0, 0, 180);
+            Slide(Vector2.up);
         }
         if (Input.GetKeyDown(KeyCode.A))
         {
-            int slideDist = GetSlideDist(Vector2.left);
-            transform.position = transform.position + Vector3.left * slideDist;
             transform.rotation = Quaternion.Euler(0, 0, 270);
+            Slide(Vector2.left);
         }
         if (Input.GetKeyDown(KeyCode.S))
         {
-            int slideDist = GetSlideDist(Vector2.down);
-            transform.position = transform.position + Vector3.down * slideDist;
             transform.rotation = Quaternion.Euler(0, 0, 0);
+            Slide(Vector2.down);
         }
         if (Input.GetKeyDown(KeyCode.D))
         {
-            int slideDist = GetSlideDist(Vector2.right);
-            transform.position = transform.position + Vector3.right * slideDist;
             transform.rotation = Quaternion.Euler(0, 0, 90);
+            Slide(Vector2.right);
         }
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Spacebar");
-            Vector3 pushDir = Explosion();
-            transform.position = transform.position + pushDir;
             Jump();
         }
     }
 
     #region My Functions
-    
-    // Can make less redundant if this function updates the object's transform.
-    private int GetSlideDist(Vector2 slideDir)
-    { // Raycasts in input direction, returns slide distance
+
+    private void Slide(Vector2 slideDir)
+    {
         groundDir = slideDir;
         if (!Physics2D.Raycast(transform.position, slideDir, 100)) // Shoot into the void
         {
             Debug.Log("YOU DIEDED");
-            return 100;
+            return;
         }
         int rayLength = 1;
-        while (!Physics2D.Raycast(transform.position, slideDir, rayLength)) {
+        while (!Physics2D.Raycast(transform.position, slideDir, rayLength))
+        {
             rayLength++;
         }
-        return rayLength - 1;
+        int slideDist = rayLength - 1;
+        transform.position = transform.position + new Vector3(slideDir.x, slideDir.y, 0) * slideDist;
     }
 
-    // Will probably get rid of this method in favor of 'Jump', because Jump is more fun and intuitive
-    private Vector2 Explosion()
-    { // Raycasts in 4 directions, returns vector of push-off direction
-        int pushX = 0;
-        int pushY = 0;
-        if (Physics2D.Raycast(transform.position, Vector2.right, 1))
-            pushX--;
-        if (Physics2D.Raycast(transform.position, Vector2.left, 1))
-            pushX++;
-        if (Physics2D.Raycast(transform.position, Vector2.up, 1))
-            pushY--;
-        if (Physics2D.Raycast(transform.position, Vector2.down, 1))
-            pushY++;
-        Vector3 pushDir = new Vector3(pushX, pushY, 0);
-        if (Physics2D.Raycast(transform.position, pushDir, 1))
-        {
-            return new Vector3(0, 0, 0);
-        }
-        Debug.Log("Pushing in Direction " + pushDir);
-        return pushDir;
-    }
-
-    private void Jump()
+    private void Jump() // Jump 1 space off of wall
     {
-        Debug.Log("Ground is in " + groundDir);
-        // Shoot raycast in ground direction
-        // if we hit something, check what we hit
-        // if we hit a jumpable object, try to jump
-        // shoot raycast in opposite ground direction
-        // if the square is empty, jump
+        if (Physics2D.Raycast(transform.position, groundDir, 1) && !Physics2D.Raycast(transform.position, -groundDir, 1))
+        {
+            // if we hit a jumpable object, jump
+            transform.position = transform.position + new Vector3(-groundDir.x, -groundDir.y, 0);
+        }
     }
 
     #endregion
