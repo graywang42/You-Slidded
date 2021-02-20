e48e19b3db1cc69d3671f99a4388a7d3e85af8ee
graywang42 <graywang42@gmail.com>
2021-02-20 15:32:16 -0800

    Revised lots of stuff, but basically, sliding now essentially works via eventmanager and movement script

---
+++ You Slidded/Assets/Scripts/EventManager.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D.Path.GUIFramework;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public static EventManager what;
+
+    private void Awake()
+    {
+        what = this; // What is this for
+    }
+
+    public event Action pressW; // Create and name the action
+    public event Action pressA;
+    public event Action pressS;
+    public event Action pressD;
+    public event Action prune;
+    public event Action updateDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (pressW != null) // Make sure the action is not null (I think this means it has no subscribers, which creates an error if null)
+            {
+                pressW(); // Trigger the event
+            }
+            if (prune != null)
+            {
+                prune();
+            }
+            if (updateDirection != null)
+            {
+                updateDirection();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (pressA != null) // Make sure the action is not null (I think this means it has no subscribers, which creates an error if null)
+            {
+                pressA(); // Trigger the event
+            }
+            if (prune != null)
+            {
+                prune();
+            }
+            if (updateDirection != null)
+            {
+                updateDirection();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (pressS != null) // Make sure the action is not null (I think this means it has no subscribers, which creates an error if null)
+            {
+                pressS(); // Trigger the event
+            }
+            if (prune != null)
+            {
+                prune();
+            }
+            if (updateDirection != null)
+            {
+                updateDirection();
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (pressD != null) // Make sure the action is not null (I think this means it has no subscribers, which creates an error if null)
+            {
+                pressD(); // Trigger the event
+            }
+            if (prune != null)
+            {
+                prune();
+            }
+            if (updateDirection != null)
+            {
+                updateDirection();
+            }
+        }
+    }
+}
---
+++ You Slidded/Assets/Scripts/Movement.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Movement : MonoBehaviour
+{
+    private Vector2Int groundDir;
+    private GridLayout gridLayout;
+    private Tilemap tilemap;
+    private Vector3Int cellPosition;
+    public bool hasSlidded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Events
+        EventManager.what.pressW += pressW;
+        EventManager.what.pressA += pressA;
+        EventManager.what.pressS += pressS;
+        EventManager.what.pressD += pressD;
+        EventManager.what.prune += prune;
+        EventManager.what.updateDirection += updateDirection;
+
+        // Initialize
+        groundDir = Vector2Int.down;
+        gridLayout = GameObject.Find("Grid").GetComponent<GridLayout>();
+        tilemap = gridLayout.GetComponentInChildren<Tilemap>();
+        hasSlidded = false;
+        UpdateCellPosition();
+    }
+
+    #region Event Triggers
+    private void pressW()
+    {
+        Slide(Vector2Int.up);
+    }
+    private void pressA()
+    {
+        Slide(Vector2Int.left);
+    }
+    private void pressS()
+    {
+        Slide(Vector2Int.down);
+    }
+    private void pressD()
+    {
+        Slide(Vector2Int.right);
+    }
+    private void prune()
+    {
+        Debug.Log("Prune test");
+        transform.DetachChildren();
+    }
+    private void updateDirection()
+    {
+        if (groundDir == Vector2Int.up)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 180);
+        }
+        if (groundDir == Vector2Int.left)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 270);
+        }
+        if (groundDir == Vector2Int.down)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        if (groundDir == Vector2Int.right)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 90);
+        }
+    }
+    #endregion
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Slide Method
+    private void Slide(Vector2Int slideDir)
+    {
+        Debug.Log("Sliding " + transform);
+        UpdateCellPosition();
+        groundDir = slideDir;
+
+        // Death Check
+        if (!Physics2D.Raycast(transform.position, slideDir, 100)) // Shoot into the void
+        {
+            Debug.Log("YOU DIEDED");
+            return;
+        }
+
+        // Stepping Raycast
+        int rayLength = 1;
+        while (!Physics2D.Raycast(transform.position, slideDir, rayLength))
+        {
+            rayLength++;
+            // Debug.Log(rayLength);
+        }
+        int slideDist = rayLength - 1;
+
+        // Check tile
+        TileBase tile = GetTileHit(slideDir, rayLength);
+        if (tile == null) // We hit a non-tile object
+        {
+            // Get gameobject hit
+            RaycastHit2D hit = (Physics2D.Raycast(transform.position, slideDir, rayLength));
+            Debug.Log("We hit tile " + hit.transform);
+            transform.parent = hit.transform;
+            transform.position = hit.transform.position - (Vector3Int)slideDir;
+
+        } else // We hit a tile
+        {
+            Debug.Log("Hit tile");
+            if (tile.name == "Spikes")
+            {
+                Debug.Log("YOU DIEDED");
+            }
+            if (tile.name == "Goal")
+            {
+                Debug.Log("YOU WONDED");
+            }
+            transform.position = transform.position + (Vector3Int)slideDir * slideDist;
+        }
+
+        UpdateCellPosition();
+    }
+
+    #region Smaller Functions
+    // Turn world position to grid position
+    private void UpdateCellPosition()
+    {
+        cellPosition = gridLayout.WorldToCell(transform.position);
+    }
+
+    // Returns tile in direction tileDir, distance tileDist, relative to You
+    private TileBase GetTileHit(Vector2Int tileDir, int tileDist)
+    {
+        TileBase tile = tilemap.GetTile(cellPosition + (Vector3Int)tileDir * tileDist);
+        return tile;
+    }
+    #endregion
+}
