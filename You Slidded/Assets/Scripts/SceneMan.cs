141abd960bd6d7dcd9eca02911fbcb178300882f
graywang42 <graywang42@gmail.com>
2021-02-24 22:00:45 -0800

    Push space to advance to next level, after reaching goal

--- You Slidded/Assets/Scripts/SceneMan.cs
+++ You Slidded/Assets/Scripts/SceneMan.cs
@@ -12,6 +12,7 @@ public class SceneMan : MonoBehaviour
     private Tilemap tilemap;
     public GameObject You;
     public GameObject Block;
+    private bool hasWonded;
 
     // Start is called before the first frame update
     void Start()
@@ -21,9 +22,17 @@ public class SceneMan : MonoBehaviour
 
         grid = GameObject.Find("Grid").GetComponent<GridLayout>();
         tilemap = grid.GetComponentInChildren<Tilemap>();
+        hasWonded = false;
         SpawnObjects();
     }
 
+    private void Update()
+    {
+        if (hasWonded && Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
 
     private void SpawnObjects()
     {
@@ -55,8 +64,8 @@ public class SceneMan : MonoBehaviour
 
     private void YouWonded()
     {
-        Debug.Log("YOU WONDED");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("YOU WONDED - Press Space to advance to the next level");
+        hasWonded = true;
         // Advance to the next scene or return to some kind of level select hub
     }
 }
